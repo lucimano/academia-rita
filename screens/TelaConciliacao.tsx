cb1da4f60fbd7499adaf3682113a480e95583275
Luciano H S Luz <luciano.luz@unesp.br>
2026-01-14 03:45:17 -0300

    concerto

--- screens/TelaConciliacao.tsx
+++ screens/TelaConciliacao.tsx
@@ -3,7 +3,7 @@ import React, { useState } from 'react';
 import { View, Text, StyleSheet, TouchableOpacity, Alert, ScrollView, ActivityIndicator } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import * as DocumentPicker from 'expo-document-picker';
-import * as FileSystem from 'expo-file-system';
+import * as FileSystem from 'expo-file-system/legacy';
 import axios from 'axios';
 
 export default function TelaConciliacao({ navigation }) {
